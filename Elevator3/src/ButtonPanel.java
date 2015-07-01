d7900286270300e1dcc1f8bd60a0c7f2c3c13e47
piyu2115 <priyushet@gmail.com>
2015-07-01 11:42:32 +0530

    commit wed'day

--- Elevator3/src/ButtonPanel.java
+++ Elevator3/src/ButtonPanel.java
@@ -2,8 +2,8 @@
 public class ButtonPanel {
 	private String btnValue;
 
-	DoorButton db=new DoorButton();
-	FloorButton fb=new FloorButton();
+	//DoorButton db=new DoorButton();
+	//FloorButton fb=new FloorButton();
 	public String getBtnValue() {
 		return btnValue;
 	}
--- Elevator3/src/FloorButton.java
+++ Elevator3/src/FloorButton.java
@@ -2,12 +2,5 @@
 public class FloorButton extends ButtonPanel {
 
 	
-	public void openButtonSelected(Door door){
-		door.openDoor();
-	}
-	
-	public void closeButtonSelected(Door door){
-			door.closeDoor();
-		}
 	
 }
