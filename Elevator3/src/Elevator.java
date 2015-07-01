c3f278860ff4205e39665a893f267081a9373553
WilberLobo <lobowilber07@gmail.com>
2015-07-01 13:09:50 +0530

    floorbtn

--- Elevator3/src/Elevator.java
+++ Elevator3/src/Elevator.java
@@ -1,26 +1,26 @@
 
 public class Elevator {
-ButtonPanel bp=new ButtonPanel(); 
-int current_floor;
-Elevator(){
-	current_floor=0;
-	display();
-}
-Elevator(int floor){
-	current_floor=floor;
-	
-}
-public void display(){
-	System.out.println("current_floor =>"+ current_floor);
-	System.out.println("select floor where you want to go");
-	System.out.println("0	1	2	3	4	5	6");
-}
+	ButtonPanel bp=new ButtonPanel(); 
+	int current_floor;
+	Elevator(){
+		current_floor=0;
+		display();
+	}
+	Elevator(int floor){
+		current_floor=floor;
 
-public void goUp(int destination){
-	current_floor++;
-}
+	}
+	public void display(){
+		System.out.println("current_floor =>"+ current_floor);
+		System.out.println("select floor where you want to go");
+		System.out.println("0	1	2	3	4	5	6");
+	}
 
-public void goDown(int destination){
-	current_floor--;
-}
+	public void goUp(int destination){
+		current_floor++;
+	}
+
+	public void goDown(int destination){
+		current_floor--;
+	}
 }
--- Elevator3/src/FloorButton.java
+++ Elevator3/src/FloorButton.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 
 public class FloorButton extends ButtonPanel {
+	int floorValue;
 
-	
-	
+	public int  selectFloor(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter floor number : ");
+		floorValue = sc.nextInt();
+		System.out.println("Floor accepted " + floorValue);
+		return  floorValue;
+	}
+
 }
